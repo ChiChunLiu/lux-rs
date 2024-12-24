fb7be4f78a66639880d2a872004006cdb270d26f
cliu <cliu@ccliu.me>
2024-12-25 02:06:58 +0800

    feat(parser): add expression and its visitor

---
+++ src/expressions.rs
@@ -0,0 +1,127 @@
+use crate::token::Token;
+use paste;
+
+enum LiteralValue<'a> {
+    String(&'a str),
+    Number(f64),
+}
+
+trait Accept<R> {
+    fn accept(&self, visitor: &dyn ExprVisitor<R>) -> R;
+}
+
+trait ExprVisitor<R> {
+    fn visit_binary_expr(&self, expr: &BinaryExpr) -> R;
+    fn visit_unary_expr(&self, expr: &UnaryExpr) -> R;
+    fn visit_literal_expr(&self, expr: &LiteralExpr) -> R;
+    fn visit_grouping_expr(&self, expr: &GroupingExpr) -> R;
+}
+
+struct AstPrinter;
+impl AstPrinter {
+    fn parenthesize(&self, name: &str, exprs: &[&Expr]) -> String {
+        let mut result = String::new();
+        result.push('(');
+        result.push_str(&format!("{}", name));
+        for expr in exprs {
+            result.push(' ');
+            result.push_str(&expr.accept(self))
+        }
+        result.push(')');
+        result
+    }
+}
+impl ExprVisitor<String> for AstPrinter {
+    fn visit_unary_expr(&self, expr: &UnaryExpr) -> String {
+        self.parenthesize(expr.operator.lexeme, &[&expr.right])
+    }
+    fn visit_binary_expr(&self, expr: &BinaryExpr) -> String {
+        self.parenthesize(expr.operator.lexeme, &[&expr.left, &expr.right])
+    }
+    fn visit_literal_expr(&self, expr: &LiteralExpr) -> String {
+        match expr.value {
+            LiteralValue::Number(n) => format!("{}", n),
+            LiteralValue::String(s) => s.to_owned(),
+        }
+    }
+    fn visit_grouping_expr(&self, expr: &GroupingExpr) -> String {
+        self.parenthesize("group", &[&expr.expr])
+    }
+}
+
+#[macro_export]
+macro_rules! ast_node {
+    ( $node_name:ident,  $(($field_name:ident, $field_type:ident)),* ) => {
+        struct $node_name<'a> {
+            $(
+                $field_name: $field_type<'a>,
+            )*
+        }
+
+        paste::paste! {
+        impl<'a, R> Accept<R> for $node_name<'a> {
+           fn accept(&self, visitor: &dyn ExprVisitor<R>) -> R {
+               visitor.[<visit_ $node_name:snake>](self)
+           }
+        }
+        }
+    };
+}
+
+ast_node!(BinaryExpr, (left, Expr), (operator, Token), (right, Expr));
+ast_node!(UnaryExpr, (operator, Token), (right, Expr));
+ast_node!(LiteralExpr, (value, LiteralValue));
+ast_node!(GroupingExpr, (expr, Expr));
+
+enum Expr<'a> {
+    Binary(Box<BinaryExpr<'a>>),
+    Unary(Box<UnaryExpr<'a>>),
+    Literal(Box<LiteralExpr<'a>>),
+    Grouping(Box<GroupingExpr<'a>>),
+}
+
+impl<'a, R> Accept<R> for Expr<'a> {
+    fn accept(&self, visitor: &dyn ExprVisitor<R>) -> R {
+        match self {
+            Self::Binary(expr) => expr.accept(visitor),
+            Self::Unary(expr) => expr.accept(visitor),
+            Self::Literal(expr) => expr.accept(visitor),
+            Self::Grouping(expr) => expr.accept(visitor),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::token::TokenType;
+
+    #[test]
+    fn test_ast_printer() {
+        let expression = Expr::Binary(Box::new(BinaryExpr {
+            left: Expr::Unary(Box::new(UnaryExpr {
+                operator: Token {
+                    token_type: TokenType::Minus,
+                    lexeme: "-",
+                    line: 1,
+                },
+                right: Expr::Literal(Box::new(LiteralExpr {
+                    value: LiteralValue::Number(123.0),
+                })),
+            })),
+            operator: Token {
+                token_type: TokenType::Star,
+                lexeme: "*",
+                line: 1,
+            },
+            right: Expr::Grouping(Box::new(GroupingExpr {
+                expr: Expr::Literal(Box::new(LiteralExpr {
+                    value: LiteralValue::String("abc"),
+                })),
+            })),
+        }));
+        let visitor = AstPrinter {};
+        let printed = expression.accept(&visitor);
+        assert_eq!(printed, "(* (- 123) (group abc))")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod expressions;
 mod reporter;
 mod scanner;
 mod token;
