01dad6edfc9c60d4892888c3c3be38008e97a308
cliu <cliu@ccliu.me>
2025-01-02 00:37:47 +0800

    feat(parser): synchronize upon parse error

--- src/parser.rs
+++ src/parser.rs
@@ -5,6 +5,15 @@ use crate::reporter::Reporter;
 use crate::statements::{ExprStmt, PrintStmt, Stmt, VarStmt};
 use crate::token::{Token, TokenType};
 
+pub struct ParseError {
+    token: Token,
+    message: String,
+}
+impl ParseError {
+    pub fn new(token: Token, message: String) -> Self {
+        Self { token, message }
+    }
+}
 // Statement grammar:
 // program        → declaration* EOF ;
 // declaration    → varDecl
@@ -69,41 +78,59 @@ impl<'a> Parser<'a> {
         matched
     }
 
-    fn consume(&mut self, token_type: TokenType, message: &str) -> &Token {
+    fn consume(&mut self, token_type: TokenType, message: &str) -> Result<&Token, ParseError> {
         if self.check(&token_type) {
-            self.advance()
+            Ok(self.advance())
         } else {
             let token = self.peek().clone();
-            self.reporter.parser_error(&token, message);
-            self.previous() // FIXME: previous() is a stub. Use Result<Token, &str> instead.
+            Err(ParseError::new(token, message.to_string()))
         }
     }
 
     fn declaration(&mut self) -> Stmt {
+        let nil_stub_stmt = Stmt::Expr(Box::new(ExprStmt {
+            expr: Expr::Literal(Box::new(LiteralExpr {
+                value: LiteralValue::Nil,
+            })),
+        }));
         if self.match_token_types(&[TokenType::Var]) {
-            self.var_declaration()
+            match self.var_declaration() {
+                Ok(stmt) => stmt,
+                Err(error) => {
+                    self.synchronize();
+                    self.reporter.parser_error(&error.token, &error.message);
+                    nil_stub_stmt
+                }
+            }
         } else {
-            self.statement()
+            match self.statement() {
+                Ok(stmt) => stmt,
+                Err(error) => {
+                    self.synchronize();
+                    self.reporter.parser_error(&error.token, &error.message);
+                    nil_stub_stmt
+                }
+            }
         }
     }
 
-    fn var_declaration(&mut self) -> Stmt {
+    fn var_declaration(&mut self) -> Result<Stmt, ParseError> {
         let name = self
-            .consume(TokenType::Identifier, "Expect variabel name.")
+            .consume(TokenType::Identifier, "Expect variable name.")?
             .clone();
         let initializer = if self.match_token_types(&[TokenType::Equal]) {
-            Some(self.expression())
+            Some(self.expression()?)
         } else {
             None
         };
         self.consume(
             TokenType::Semicolon,
             "Expect ';' after variable declaration.",
-        );
-        Stmt::Var(Box::new(VarStmt { name, initializer }))
+        )?;
+        Ok(Stmt::Var(Box::new(VarStmt { name, initializer })))
     }
 
-    fn statement(&mut self) -> Stmt {
+    fn statement(&mut self) -> Result<Stmt, ParseError> {
         if self.match_token_types(&[TokenType::Print]) {
             self.print_statement()
         } else {
@@ -111,38 +138,38 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn print_statement(&mut self) -> Stmt {
-        let value = self.expression();
-        self.consume(TokenType::Semicolon, "Expect ';' after value.");
-        Stmt::Print(Box::new(PrintStmt { expr: value }))
+    fn print_statement(&mut self) -> Result<Stmt, ParseError> {
+        let value = self.expression()?;
+        self.consume(TokenType::Semicolon, "Expect ';' after value.")?;
+        Ok(Stmt::Print(Box::new(PrintStmt { expr: value })))
     }
 
-    fn expression_statement(&mut self) -> Stmt {
-        let expr = self.expression();
-        self.consume(TokenType::Semicolon, "Expect ';' after value.");
-        Stmt::Expr(Box::new(ExprStmt { expr }))
+    fn expression_statement(&mut self) -> Result<Stmt, ParseError> {
+        let expr = self.expression()?;
+        self.consume(TokenType::Semicolon, "Expect ';' after value.")?;
+        Ok(Stmt::Expr(Box::new(ExprStmt { expr })))
     }
 
-    fn expression(&mut self) -> Expr {
+    fn expression(&mut self) -> Result<Expr, ParseError> {
         self.equality()
     }
 
-    fn equality(&mut self) -> Expr {
-        let mut expr = self.comparison();
+    fn equality(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.comparison()?;
         while self.match_token_types(&[TokenType::BangEqual, TokenType::EqualEqual]) {
             let operator = self.previous().clone();
-            let right = self.comparison();
+            let right = self.comparison()?;
             expr = Expr::Binary(Box::new(BinaryExpr {
                 left: expr,
                 operator,
                 right,
             }))
         }
-        expr
+        Ok(expr)
     }
 
-    fn comparison(&mut self) -> Expr {
-        let mut expr = self.term();
+    fn comparison(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.term()?;
 
         while self.match_token_types(&[
             TokenType::Greater,
@@ -151,55 +178,55 @@ impl<'a> Parser<'a> {
             TokenType::LessEqual,
         ]) {
             let operator = self.previous().clone();
-            let right = self.term();
+            let right = self.term()?;
             expr = Expr::Binary(Box::new(BinaryExpr {
                 left: expr,
                 operator,
                 right,
             }))
         }
-        expr
+        Ok(expr)
     }
 
-    fn term(&mut self) -> Expr {
-        let mut expr = self.factor();
+    fn term(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.factor()?;
         while self.match_token_types(&[TokenType::Minus, TokenType::Plus]) {
             let operator = self.previous().clone();
-            let right = self.factor();
+            let right = self.factor()?;
             expr = Expr::Binary(Box::new(BinaryExpr {
                 left: expr,
                 operator,
                 right,
             }))
         }
-        expr
+        Ok(expr)
     }
 
-    fn factor(&mut self) -> Expr {
-        let mut expr = self.unary();
+    fn factor(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.unary()?;
         while self.match_token_types(&[TokenType::Slash, TokenType::Star]) {
             let operator = self.previous().clone();
-            let right = self.unary();
+            let right = self.unary()?;
             expr = Expr::Binary(Box::new(BinaryExpr {
                 left: expr,
                 operator,
                 right,
             }));
         }
-        expr
+        Ok(expr)
     }
 
-    fn unary(&mut self) -> Expr {
+    fn unary(&mut self) -> Result<Expr, ParseError> {
         if self.match_token_types(&[TokenType::Bang, TokenType::Minus]) {
             let operator = self.previous().clone();
-            let right = self.unary();
-            Expr::Unary(Box::new(UnaryExpr { operator, right }))
+            let right = self.unary()?;
+            Ok(Expr::Unary(Box::new(UnaryExpr { operator, right })))
         } else {
-            self.primary().unwrap()
+            self.primary()
         }
     }
 
-    fn primary(&mut self) -> Result<Expr, &str> {
+    fn primary(&mut self) -> Result<Expr, ParseError> {
         match &self.peek().token_type {
             TokenType::False => {
                 self.advance();
@@ -235,18 +262,24 @@ impl<'a> Parser<'a> {
             }
             TokenType::LeftParen => {
                 self.advance();
-                let expr = self.expression();
+                let expr = self.expression()?;
                 let token = self.advance();
                 match token.token_type {
                     TokenType::RightParen => Ok(Expr::Grouping(Box::new(GroupingExpr { expr }))),
-                    _ => Err("Parsing error: expecting ')'"),
+                    _ => Err(ParseError::new(
+                        self.peek().clone(),
+                        "Parsing error: expecting ')'".to_string(),
+                    )),
                 }
             }
             TokenType::Identifier => {
                 let token = self.advance().clone();
                 Ok(Expr::Variable(Box::new(VarExpr { name: token })))
             }
-            _ => Err("parsing error"),
+            _ => Err(ParseError::new(
+                self.peek().clone(),
+                "No other literal token types . Not reachable.".to_string(),
+            )),
         }
     }
 
@@ -256,5 +289,31 @@ impl<'a> Parser<'a> {
             statements.push(self.declaration())
         }
         statements
+    }
+
+    /// March forward until the beginning of the next statement. Used for
+    /// recovering from an parser error.
+    fn synchronize(&mut self) {
+        self.advance();
+        while !self.is_at_end() {
+            if self.previous().token_type == TokenType::Semicolon {
+                break;
+            }
+
+            if matches!(
+                self.peek().token_type,
+                TokenType::Class
+                    | TokenType::Fun
+                    | TokenType::Var
+                    | TokenType::For
+                    | TokenType::If
+                    | TokenType::While
+                    | TokenType::Print
+                    | TokenType::Return
+            ) {
+                break;
+            }
+            self.advance();
+        }
     }
 }
